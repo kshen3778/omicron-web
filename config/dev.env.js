31fdeb7d21e79b6bf2fd70532f796132689b558a
kshen3778 <kshen3778@gmail.com>
2018-08-28 21:38:03 -0400

    configured env variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,11 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_KEY: '"AIzaSyATpcTwZcBsSG9OcBKgGL1mNvfU0bXu1SI"',
+  AUTH_DOMAIN: '"vue-auth-cd56e.firebaseapp.com"',
+  DATABASE_URL: '"https://vue-auth-cd56e.firebaseio.com"',
+  PROJECT_ID: '"vue-auth-cd56e"',
+  STORAGE_BUCKET: '""',
+  MESSAING_SENDER_ID: '"733126669746"'
 })
